db87ca03f0535e3e532ef73304be3a7fddedb5fa
zy15025161876 <1938790964@qq.com>
2021-10-24 10:18:41 +0800

    第三

---
+++ mymall-coupon/src/test/java/com/zhou/mymallcoupon/MymallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhou.mymallcoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
